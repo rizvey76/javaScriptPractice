46ecf772ffbee61f6e9a44287c4b63039556fb7e
Gazi Rizveykhar Zaman <rizvey76zaman@gmail.com>
2023-10-03 01:25:30 +0600

    transection_observer

---
+++ Intersection_observer/example/script.js
@@ -0,0 +1,17 @@
+const squares=document.querySelectorAll("div");
+
+const observer=new IntersectionObserver(
+    (squares)=>{
+        squares.forEach(
+            (square)=>{
+                if(square.isIntersecting){
+                    square.target.classList.add("visible");
+                }else{
+                    square.target.classList.remove("visible");
+                }
+            }
+        );
+    }
+);
+
+squares.forEach((square)=> observer.observe(square));
